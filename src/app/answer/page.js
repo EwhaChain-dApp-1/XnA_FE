27f5ff1346f0cc8c6589b88643276a5cae08e277
SeoJimin <jnuni1004@naver.com>
2025-04-16 12:33:08 +0900

    feat/#9/questionDetailPage

---
+++ src/app/answer/page.js
@@ -0,0 +1,5 @@
+'use client';
+
+export default function Answer() {
+  return <div>This is Answer!</div>;
+}
---
+++ src/app/question-list/page.js
@@ -0,0 +1,5 @@
+'use client';
+
+export default function QuestionList() {
+  return <div>This is Question List!</div>;
+}
